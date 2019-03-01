561c1ff9b6d66f51268c8fb783b59eea01ad683a
badCompany55 <zachirvin45@gmail.com>
2019-03-01 17:38:09 -0600

    GET SINGLE POST add single post ability via params id

--- postRouter.js
+++ postRouter.js
@@ -1,18 +1,35 @@
-const express = require("express")
+const express = require("express");
 
-const posts = require("./data/db.js")
+const posts = require("./data/db.js");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-	try {
-		const allPosts = await posts.find()
-		console.log(allPosts)
-		res.status(200).json(posts)
-	} catch(err) {
-		res.status(500).json({error: "The posts information could not be retrieved"})
-	}
-})
+  try {
+    const allPosts = await posts.find();
+    res.status(200).json(allPosts);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The posts information could not be retrieved" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await posts.findById(req.params.id);
+    console.log(post.length);
+    post.length > 0
+      ? res.status(200).json(post)
+      : res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be retrieved." });
+  }
+});
 
 // router.post("/", async (req, res) => {
 // 	try [
